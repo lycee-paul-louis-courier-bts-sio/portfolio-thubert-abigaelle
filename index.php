4dc45318cdca280a3211d7776f187fa972b9b490
Abi <abi.thubert@gmail.com>
2026-05-21 21:51:19 +0200

    la description dans <head> était pas bonne

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <meta name="description" content="Portfolio de Abigaëlle Thubert, élève en 2ème année de BTS SIO option SLAM2">
+        <meta name="description" content="Portfolio de Abigaëlle Thubert, élève en 2ème année de BTS SIO option SLAM">
         <title>Portfolio d'Abigaëlle Thubert</title>
         <link rel="icon" type="image/png" href="assets/images/logo.png">
 
